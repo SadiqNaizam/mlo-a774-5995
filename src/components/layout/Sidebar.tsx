698a9914f5ff5d467cb088913bcdbd70489c5b7b
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-24 02:52:35 +0530

    Files generated: Sidebar.tsx

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SidebarNav from '../Dashboard/SidebarNav';
+
+const Sidebar: React.FC = () => {
+  return (
+    <div className="h-full bg-card">
+      <SidebarNav />
+    </div>
+  );
+};
+
+export default Sidebar;
